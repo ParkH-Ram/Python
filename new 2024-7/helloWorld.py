8a71461be3457010f83be8043fbc870165ec8a11
Ram <parkeryangga@gmail.com>
2024-07-30 23:12:59 +0900

    Update helloWorld.py

--- new 2024-7/helloWorld.py
+++ new 2024-7/helloWorld.py
@@ -2,3 +2,29 @@ a = 5
 b = 4
 
 print(a + b);
+
+'''`
+아래는 입력하는 코드입니다. 따라해 주셈
+'''
+
+# 제곱 연산자 
+print(a ** b)  
+
+# 몫을 구하는 연산자 
+# 파이썬은 몫을 구하려면 // 두개,  / 한개는 나누기 임
+print(a // b) 
+
+
+
+s = "Life is too short, You need Python"
+
+t = "151515"
+
+print(type(s))
+print(type(t))
+
+a = s[0:4]
+print(a)
+
+b = s[::-2]
+print(b)
