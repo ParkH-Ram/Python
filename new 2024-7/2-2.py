f7b6b4be8bcdf440bc1f9defc41ed669cd51c2b6
Ram <parkeryangga@gmail.com>
2024-08-01 23:42:53 +0900

    생각보다 미팅이 길어져서  공부 못함... ㅠㅠ

--- new 2024-7/2-2.py
+++ new 2024-7/2-2.py
@@ -3,3 +3,28 @@ number = 5
 a = "I eat %.03f apples." % number
 
 print(a)
+
+# 최신 포맷팅
+
+name = '드라이'
+
+age = '비밀'
+
+a = f'나의 이름은 {name} 입니다. 나이는 {age} 입니다.'
+
+print(a)
+
+age = 30
+
+a = f'나는 내년이면 {age+ 5  }살이 된다.'
+print(a)
+
+a = f'{"hi":>10}'
+
+print(a)
+
+
+# 예시 사용
+number = 1234567890
+formatted_number = f"{number:,}"
+print(formatted_number)  # 출력: 1,234,567,890
