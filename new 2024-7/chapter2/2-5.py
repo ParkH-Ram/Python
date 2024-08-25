f6371b7aab6c72d2564a836e15b1887936b14622
Ram <parkeryangga@gmail.com>
2024-08-25 17:25:26 +0900

    Update 2-5.py

--- new 2024-7/chapter2/2-5.py
+++ new 2024-7/chapter2/2-5.py
@@ -1,30 +1,41 @@
 dic = {'name' : 'ram', 'tall' : '187'}
 
 
-## 새로운 값 추가
-     # key             #vlaue
+ ## 새로운 값 추가
+#      # key             #vlaue
 dic['라이징 스타'] =  '이름이 뭐지?'
 
 
 dic[4] = (1,2,3)
 
 
-## 삭제 
-del(dic[4])  ## key를 선택해 삭제
+# ## 삭제 
+# del(dic[4])  ## key를 선택해 삭제
 
-print(dic)
+# print(dic)
 
-## 여러값 삭제
-#del dic['name'], dic['tall']
+# ## 여러값 삭제
+# #del dic['name'], dic['tall']
 
-print(dic)
+# print(dic)
 
 
-print(dic['name'])
-print(dic['tall'])
+# print(dic['name'])
+# print(dic['tall'])
 
-## dic.keys() ->> 키들만 dict 형태로 만들어 주는 함수
-print(dic.keys())
+# ## dic.keys() ->> 키들만 dict 형태로 만들어 주는 함수
+# print(dic.keys())
 
-for k in dic.keys() : 
-  print(k)
+# for k in dic.keys() : 
+#   print(k)
+
+
+
+# print(dic.values())
+
+
+print(dic.items())
+
+print(dic.get('name'))
+
+print(dic.get('what'))
